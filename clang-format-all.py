24d754af46a2fbf68f14a87644521ab70245a794
boomboomjassi <105042297+boomboomjassi@users.noreply.github.com>
2022-12-06 13:27:03 +0530

    Update clang-format-all.py

--- clang-format-all.py
+++ clang-format-all.py
@@ -2,15 +2,33 @@
 import subprocess
 import os
 import sys
+
 pypath = os.path.dirname(sys.executable)
 print("pypath=", pypath)
-files = [f for f in os.listdir(".") if os.path.isfile(f)]
-files_filtered = [f for f in files if ".cpp" in f]
-for f in files_filtered:
-    print("Formatting", f)
+
+
+def format_all_files_under(direc="."):
+    print("Formatting all .cpp files directly under : {}".format(direc))
+    files = [f for f in os.listdir(direc) if os.path.isfile(direc + "\\" + f)]
+    files_filtered = [f for f in files if ".cpp" in f]
+    for f in files_filtered:
+        print("Formatting", f)
+        try:
+            subprocess.run(["clang-format", "-i", direc + "\\" + f])
+            print("Successfully Formated", f)
+        except Exception as e:
+            print("Failed Formated", f)
+            print(e)
+
+
+format_all_files_under()
+folders = []
+for f in os.scandir("."):
+    if "." not in f.name:
+        folders.append(os.path.abspath(f))
+
+for folder in folders:
     try:
-        subprocess.run(["clang-format", "-i", f])
-        print("Successfully Formated", f)
+        format_all_files_under(folder + "\\")
     except Exception as e:
-        print("Failed Formated", f)
         print(e)
